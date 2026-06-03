5ae2c0fe5aede3374a22730c822465a39ca58908
Nedim <nedim_mulahusic@yahoo.com>
2026-06-03 13:13:53 -0700

    Add layout route with App and implement nested routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Outlet } from 'react-router'
 
 import './App.css'
 
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <>
-     
+     <Outlet />
     </>
   )
 }
--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -1,19 +1,19 @@
 import Home from "../pages/Home"
 import Shop from "../pages/Shop"
 import Cart from "../pages/Cart"
+import App from "../App"
+
 const routes = [
     {
         path: "/",
-        element: <Home />
+        element: <App />,
+        children: [
+            {index: true, element: <Home />},
+            {path: "shop", element: <Shop />},
+            {path: "cart", element: <Cart />},
+        ]
     },
-    {
-        path: "/shop",
-        element: <Shop />
-    },
-    {
-        path: "/cart",
-        element: <Cart />
-    },
+    
 
 ]
 
