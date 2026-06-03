3a122aa96c4d9cb786f7b3f730d42068dc0c57ef
Nedim <nedim_mulahusic@yahoo.com>
2026-06-03 13:00:06 -0700

    Created routes
    Created routes to home, shop and cart pages. Good to go

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import routes from './routes/route.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter(routes);
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+
+)
---
+++ src/routes/route.jsx
@@ -0,0 +1,20 @@
+import Home from "../pages/Home"
+import Shop from "../pages/Shop"
+import Cart from "../pages/Cart"
+const routes = [
+    {
+        path: "/",
+        element: <Home />
+    },
+    {
+        path: "/shop",
+        element: <Shop />
+    },
+    {
+        path: "/cart",
+        element: <Cart />
+    },
+
+]
+
+export default routes
