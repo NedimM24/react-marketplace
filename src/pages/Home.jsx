204c3f2acf7a7b25613d9445d55b5f3d2f1214f7
Nedim <nedim_mulahusic@yahoo.com>
2026-06-05 09:11:37 -0700

    Start home page layout
    Create Home page layout with store image and company bio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
+import shopIMage from "../assets/shop.jpeg"
 export default function Home(){
     return(
         <>
-            <h1>Home</h1>
+            <main> {/* container */}
+                <section> {/* top section */}
+                    <img src={shopIMage} alt="Picture of a retail store" />
+                </section>
+
+                <section> {/* bottom section */}
+                    <p>About Us</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis quidem blanditiis maiores possimus alias repudiandae tenetur sequi dolore illum! Similique unde ea architecto iure numquam assumenda tempore natus vel ipsum.</p>
+
+                </section>
+            </main>
         </>
     )
 }
