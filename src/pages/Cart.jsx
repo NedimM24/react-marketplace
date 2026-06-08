3a150364ef8a4d632325b6ab93fd879c368bd984
Nedim <nedim_mulahusic@yahoo.com>
2026-06-08 10:29:22 -0700

    Empty cart conditional
    If cart is empty, display message on cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,13 +1,25 @@
 import { useOutletContext } from "react-router-dom";
 import { CartItem } from "../components/CartItem";
 import styles from "./Cart.module.css";
+import { Link } from "react-router-dom";
 
 export default function Cart() {
     const { productsArray, cart, setCart } = useOutletContext();
     const total = cart.reduce((sum, item) => sum + item.price, 0);
 
+    if(!cart || cart.length === 0) {
+        return(
+            <div>
+                <h1>Cart is Empty!</h1>
+                <h1>Get to shopping!</h1>
+                <button><Link className={styles.links} to={'/shop'}>Shop</Link></button>
+            </div>
+        )
+    }
+
     return (
         <>
+            
             <div className={styles.container}> {/* Container */}
 
                 <div className={styles.left}> {/* Left side contains cartitems */}
