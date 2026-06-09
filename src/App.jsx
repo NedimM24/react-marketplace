bb768b1df9647d85cdb7a745a09e459eeb3b2311
Nedim <nedim_mulahusic@yahoo.com>
2026-06-09 09:39:46 -0700

    fetch data
    used use effect hook to fetch 12 items from fake store api and added them to a products array

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Outlet } from 'react-router'
 import Navbar from './components/Navabar'
 import { products } from './data/products'
@@ -10,8 +10,29 @@ function App() {
   //Get total quantity
   const totalQuantity = (cart.reduce((sum, item) => sum + item.quantity,0))
 
-  //State for the products array. No need to set, read only 
-  const [productsArray] = useState(products);
+  //State for the products array. No need to set, read only (SWAP NEXT)
+  //const [productsArray] = useState(products);
+
+  //new array that will be fetched, ill clear the products array and link it here
+  const [productsArray, setProductsArray] = useState([]);
+
+  //useEffect to fetch from api and display info on mount
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try{
+        const response = await fetch('https://fakestoreapi.com/products/');
+        const data = await response.json();
+        setProductsArray(data.slice(0, 12))
+      } catch (error) {
+        console.log(error);
+        
+      }
+    }
+    fetchProducts()
+  }, [])
+    
+    console.log(productsArray);
+    
 
   return (
     <>
